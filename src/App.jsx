2c3407429d3bd3c7317a96feed9edb0d7feca865
Kimberley <kimberley@Heathers-MacBook-Air.local>
2023-10-24 08:47:06 +0100

    articles page

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import { NavLink } from "react-router-dom";
 import Topics from "./components/Topics";
-import Users from "./Users";
+import Users from "./components/Users";
 import Articles from "./components/Articles";
+import Article from "./components/article";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Articles />} />
           <Route path="/articles" element={<Articles />} />
           <Route path="/users" element={<Users />} />
+          <Route path="/article/:id" component={<Article />} />
         </Routes>
       </main>
     </>
